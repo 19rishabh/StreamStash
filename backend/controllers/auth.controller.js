5cfa5ed0f96621afb9f24a45674c1ebc7ad4deb5
Rishabh <rishabhagrawal0623@gmail.com>
2024-09-30 01:17:00 +0530

    Generate JWT, login logic and logout logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 
 export async function signup(req, res){
     try {
@@ -46,28 +47,61 @@ export async function signup(req, res){
             image
         })
 
+        generateTokenAndSetCookie(newUser._id, res);
         await newUser.save();
+
         res.status(201).json({
-            success:true,user:{
+            success:true,
+            user:{
                 ...newUser._doc,
                 password:"",
             }, 
-        });
-
-
-
+        });   
     } catch (error) {
         console.log("Error in Signup Controller",error.message)
         res.status(500).json({sucess:false,message:"Internal Server Error"})
-        
     }
-    res.send("Signup route");
 }
 
 export async function login(req, res){
-    res.send("Login route");
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({success:false, message:"All fields required"})
+        }
+
+        const user = await User.findOne({email: email})
+        if (!user){
+            return res.status(400).json({success:false, message:"Invalid Credentials"})
+        }
+            const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+            if (!isPasswordCorrect) {
+                return res.status(400).json({success:false, message:"Invalid Credentials"});
+            }
+
+            generateTokenAndSetCookie(user._id, res);
+
+            res.status(200).json({
+                success:true,
+                user:{
+                    ...user._doc,
+                    password:"",
+                    }
+                })
+    } catch (error) {
+        console.log("Error in Login Controller",error.message)
+        res.status(500).json({success:false,message:"Internal Server Error"});
+    }
 }
 
 export async function logout(req, res){
-    res.send("Logout route");
+    try {
+        res.clearCookie("jwt-StreamStash");
+        res.status(200).json({success:true,message:"Logged out successfully"});
+    } catch(error) {
+        console.log("Error in Logout Controller", error.message);
+        res.status(500).json({success:false, message:"Internal Server Error"});
+    }
 }
