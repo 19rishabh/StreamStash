756275ad3f192b42e12f67a43790b6331601dafa
Aaditya0417 <aadityap@somaiya.edu>
2024-10-14 17:48:35 +0530

    Signup, Login, Logout functionality

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,14 +1,12 @@
+import { useAuthStore } from '../../store/authUser';
 import AuthScreen from './AuthScreen';
 import HomeScreen from './HomeScreen';
 
 const HomePage = () => {
-  const user = false;
+  const {user} = useAuthStore();
 
-  return (
-    <div>
-      {user ?<HomeScreen />: <AuthScreen />}
-    </div>
-  )
+  return <> {user ?<HomeScreen />: <AuthScreen />}</>
+  
 };
 
 export default HomePage;
